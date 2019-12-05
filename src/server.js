64555415e8dfd2d374d911a6e271d348e891c1d5
Bruno H. A. Fonseca <bruno3145@hotmail.com>
2019-12-05 19:47:09 -0300

    :wrench: Setting Access-control-allow-origin

--- src/server.js
+++ src/server.js
@@ -9,6 +9,15 @@ const body_parser = require('body-parser');
 // Starting API service
 const app = express();
 app.use(body_parser.json())
+app.use(function(req, res, next) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+  
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+    next();
+  
+  });
 app.disable('etag');
 app.use('/api/v1', controllers);
 
